386c7e1c0c285b8309e7daf528cf3c0f9c3d5045
omar <omar.sanchez.martinez007@gmail.com>
2024-08-28 17:11:40 -0600

    cors2

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ app.use(cors({
         if (ACCEPTED_ORIGINS.includes(origin)) {
             return callback(null, true)
         }
+
+        if (!origin) {
+            return callback(null,true)
+        }
+
+        return callback(new Error('Not allowed by cors'))
     }
 }))
 
